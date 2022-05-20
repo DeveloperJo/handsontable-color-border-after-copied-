c47dab0c908fd0b3034da97402fd543100a969a3
DevJo <97277282+DeveloperJo@users.noreply.github.com>
2022-05-20 18:11:38 +0900

    Create border_after_copy.js

---
+++ border_after_copy.js
@@ -0,0 +1,60 @@
+const container = document.querySelector('#example4');
+
+const hot = new Handsontable(container, {
+  data: [
+    ['', 'Tesla', 'Toyota', 'Honda', 'Ford'],
+    ['2018', 10, 11, 12, 13, 15, 16],
+    ['2019', 10, 11, 12, 13, 15, 16],
+    ['2020', 10, 11, 12, 13, 15, 16],
+  ],
+  rowHeaders: true,
+  colHeaders: true,
+  afterCopy: function(data, selection) {
+    const startCol = selection['0']['startCol'];
+    const startRow = selection['0']['startRow'];
+    const endCol = selection['0']['endCol'];
+    const endRow = selection['0']['endRow'];
+
+    let width = endCol - startCol;
+    width = (width < 0) ? startCol - endCol : width;
+
+    let height = endRow - startRow;
+    height = (height < 0) ? startRow - endRow : height;
+
+    hot.updateSettings({
+      customBorders:[
+        {
+          range: {
+            from: {
+              row: startRow,
+              col: startCol
+            },
+            to: {
+              row: endRow,
+              col: endCol
+            }
+          },
+          top: {
+            width: width,
+            color: 'red'
+          },
+          bottom: {
+            width: width,
+            color: 'red'
+          },
+          start: {
+            width: height,
+            color: 'red'
+          },
+          end: {
+            width: height,
+            color: 'red'
+          }
+        }
+      ]
+    });
+    
+  },
+  height: 'auto',
+  licenseKey: 'non-commercial-and-evaluation'
+});
